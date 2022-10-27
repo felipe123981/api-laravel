c815ad9c29e31c3b52991733109f7a14bd936eb3
felipe123981 <filipe.timachine@gmail.com>
2022-10-27 07:13:15 -0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::prefix('usuarios')->group(function() {
+	Route::get('', function() {
+		return 'usuario';
+	})->name('usuarios');
+
+	Route::get('/{id}', function() {
+		return 'Mostrar detalhes';
+	})->name('usuarios.show');
+
+	Route::get('/{id}/tags', function() {
+		return 'Tags de usuario';
+	})->name('usuarios.tags');
+});
+
+Route::get('/a-empresa/{string?}', function ($string = null) {
+	return $string;
+})->name('a-empresa');
+
+Route::get('/users/{paramA}/{paramB}', function ($paramA, $paramB) {
+	return $paramA . ' - ' . $paramB;
+	//    return view('welcome');
+})->name('users');
